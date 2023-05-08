0c77446019893c1d5ec710e933abe32a175cc47b
Darcless1 <dmitry.tsyplakov.works@gmail.com>
2023-05-07 20:42:38 -0700

    logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 interface Coin {
@@ -17,36 +16,39 @@ interface Coin {
   explorer: string;
 }
 
+interface CoinInfo {
+  name: string;
+  symbol: string;
+  price: string;
+}
+
 const App = (): JSX.Element => {
-  const [bitcoinPrice, setBitcoinPrice] = useState<string>("");
-  const [litecoinPrice, setLitecoinPrice] = useState<string>("");
-  const [ethereumPrice, setEthereumPrice] = useState<string>("");
-  const [dogePrice, setDogePrice] = useState<string>("");
+  const [coins, setCoins] = useState<CoinInfo[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchPrices = async (): Promise<void> => {
-      const response = await fetch("https://api.coincap.io/v2/assets");
-      const data = await response.json();
+      try {
+        const response = await fetch("https://api.coincap.io/v2/assets");
+        if (!response.ok) {
+          throw new Error("Failed to fetch prices");
+        }
+        const data = await response.json();
 
-      const bitcoin: Coin | undefined = data.data.find(
-        (item: Coin) => item.symbol === "BTC"
-      );
-      setBitcoinPrice(parseFloat(bitcoin!.priceUsd).toFixed(2));
+        const coinsData: Coin[] = data.data;
+        const coinsInfo: CoinInfo[] = coinsData.map((coin: Coin) => {
+          return {
+            name: coin.name,
+            symbol: coin.symbol,
+            price: parseFloat(coin.priceUsd).toFixed(2),
+          };
+        });
 
-      const litecoin: Coin | undefined = data.data.find(
-        (item: Coin) => item.symbol === "LTC"
-      );
-      setLitecoinPrice(parseFloat(litecoin!.priceUsd).toFixed(2));
-
-      const ethereum: Coin | undefined = data.data.find(
-        (item: Coin) => item.symbol === "ETH"
-      );
-      setEthereumPrice(parseFloat(ethereum!.priceUsd).toFixed(2));
-
-      const doge: Coin | undefined = data.data.find(
-        (item: Coin) => item.symbol === "DOGE"
-      );
-      setDogePrice(parseFloat(doge!.priceUsd).toFixed(2));
+        setCoins(coinsInfo);
+        setError(null);
+      } catch (error: any) {
+        setError(error.message);
+      }
     };
 
     fetchPrices();
@@ -56,40 +58,18 @@ const App = (): JSX.Element => {
   }, []);
 
   return (
-    <div>
+    <div className="parent_container">
       <h2>Live Prices</h2>
-      <div className="container">
-        <div className="coin-price">
-          <div>
-            <h3>$ {bitcoinPrice}</h3>
-            <h3>Bitcoin</h3>
+      {coins.slice(0, 5).map((coin: CoinInfo) => (
+        <div className="container" key={coin.symbol}>
+          <div className="coin-price">
+            <div>
+              <h3>$ {coin.price}</h3>
+              <h3>{coin.name}</h3>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container">
-        <div className="coin-price">
-          <div>
-            <h3>$ {litecoinPrice}</h3>
-            <h3>LiteCoin</h3>
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="coin-price">
-          <div>
-            <h3>$ {ethereumPrice}</h3>
-            <h3>Ethereum</h3>
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="coin-price">
-          <div>
-            <h3>$ {dogePrice}</h3>
-            <h3>Doge</h3>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
